27be1e1c1dcbfe5bb4e38743d67a7b4a001651a6
Rafael Santos <chavesrafael9@gmail.com>
2024-12-05 22:44:15 -0300

    feat: add pictures in gallery and create a carousel

---
+++ frontend/src/app/components/carousel/carousel.component.ts
@@ -0,0 +1,44 @@
+import { Component, Input, OnDestroy, AfterRenderPhase, afterNextRender, NgZone } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-carousel',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './carousel.component.html',
+  styleUrl: './carousel.component.scss'
+})
+export class CarouselComponent implements OnDestroy {
+  @Input() images: string[] = [];
+  currentSlide: number = 0;
+
+  private timer: any;
+
+  constructor(ngZone: NgZone) {
+    this.startAutoSlide(ngZone);
+  }
+
+  ngOnDestroy() {
+    clearInterval(this.timer); // Limpa o timer ao destruir o componente
+  }
+
+  startAutoSlide(ngZone: NgZone) {
+    afterNextRender(() => {
+      ngZone.run(() => {
+        this.timer = setInterval(() => {
+          this.nextSlide();
+        }, 5000); // Troca a cada 5 
+      });
+    }, { phase: AfterRenderPhase.Write });
+  }
+
+  nextSlide(): void {
+    this.currentSlide = (this.currentSlide + 1) % this.images.length;
+  }
+
+  prevSlide(): void {
+    this.currentSlide = (this.currentSlide - 1 + this.images.length) % this.images.length;
+  }
+}
+
+
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { RouterLink } from '@angular/router';
+import { CarouselComponent } from "../../components/carousel/carousel.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, RouterLink],
+  imports: [HeaderComponent, FooterComponent, RouterLink, CarouselComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  carouselImages: string[] =
+    ["assets/gallery/aereo-bowl-mute.jpg",
+      "assets/gallery/backside-torque-corrimao-rua.jpg",
+      "assets/gallery/pedigree-bowl.jpg",
+      "assets/gallery/soul-corrimao-rua.jpg",
+      "assets/gallery/topsoul-corrimao-rua.jpg"
+    ]
 
 }
