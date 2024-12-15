cd77ad6d2e8ab0a27fbd931dc10e4570f7a3db8d
Rafael Santos <chavesrafael9@gmail.com>
2024-12-15 17:39:34 -0300

    chore: remove produts from header and adjusts in sponsorship text

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -11,11 +11,13 @@ export class AppComponent {
   constructor(private router: Router) { }
 
   ngOnInit(): void {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        window.scrollTo(0, 0); 
-      }
-    });
+    if (typeof window !== 'undefined') {
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          window.scrollTo(0, 0);
+        }
+      });
+    }
   }
 
 }
